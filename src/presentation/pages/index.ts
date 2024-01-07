f867178fe9d64d2c7969df6ec3cb62ac59acad7f
juancalderonx <juanescalderon12@gmail.com>
2024-01-07 15:31:16 -0500

    feat(): router config finished

---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,9 @@
+export { AssistantPage } from "./assistant/AssistantPage";
+export { AudioToTextPage } from "./audio-to-text/AudioToTextPage";
+export { ImageGenerationPage } from "./image-generation/ImageGenerationPage";
+export { ImageTunningPage } from "./image-tunning/ImageTunningPage";
+export { OrthographyPage } from "./orthography/OrthographyPage";
+export { ProsConsStreamPage } from "./pros-cons-stream/ProsConsStreamPage";
+export { ProsConsPage } from "./pros-cons/ProsConsPage";
+export { TextToAudioPage } from "./text-to-audio/TextToAudioPage";
+export { TranslatePage } from "./translate-page/TranslatePage";
